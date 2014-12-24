64e19b3ff641fec919e62334e34bc597403e6238
provencher <eprovencher92@gmail.com>
2014-12-24 14:07:20 -0500

    Project Files for Basic language features

---
+++ Test1/src/classTest.py
@@ -0,0 +1,21 @@
+__author__ = 'Eric'
+
+
+
+#Class
+class car():
+    wheels = 4
+
+    #string = "There are " + str(wheels) + "wheels";
+
+    def traits(self):
+        print("YOLO");
+
+
+def main():
+
+    car1 = car();
+    car1.traits();
+
+
+
